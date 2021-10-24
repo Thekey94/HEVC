637a4fd2b0d28995adbf671dc472f50820677a3a
Thekey94 <ahmedashrafzaky@gmail.com>
2021-10-24 05:22:27 +0200

    ADC Initialization and Pilot reaction implementation

--- Core/Inc/Pilot_Cntr.h
+++ Core/Inc/Pilot_Cntr.h
@@ -12,9 +12,23 @@
 #include "stm32f1xx_hal.h"
 
 /* Private Defines -----------------------------------------------------------*/
-#define PWM_PIN			GPIO_PIN_5
-#define PWM_PIN_BANK	GPIOB
+#define PWM_PIN							GPIO_PIN_5
+#define PWM_PIN_BANK					GPIOB
 
+#define ADC_MAX_VALUE					4095
+#define VCC_IN							3300
+#define EV_NOT_CONNECTED_VOLTAGE		(VCC_IN)
+#define EV_CONNECTED_VOLTAGE			(VCC_IN/4) * 3
+#define EV_CHARGING_VOLTAGE				(VCC_IN/4) * 2
+#define EV_CHARGING_VENT_REQ_VOLTAGE	(VCC_IN/4) * 1
+#define EV_ERROR_VOLTAGE				(VCC_IN/4) * 0
+#define EV_UNKNOWN_VOLTAGE				6
+
+
+#define MIN_NOT_CONNECTED_VOLTAGE 		(EV_NOT_CONNECTED_VOLTAGE+EV_CONNECTED_VOLTAGE)/2
+#define MIN_CONNECTED_VOLTAGE			(EV_CONNECTED_VOLTAGE+EV_CHARGING_VOLTAGE)/2
+#define MIN_CHARGING_VOLTAGE			(EV_CHARGING_VOLTAGE+EV_CHARGING_VENT_REQ_VOLTAGE)/2
+#define MIN_VENT_REQ_VOLTAGE			(EV_CHARGING_VENT_REQ_VOLTAGE+EV_ERROR_VOLTAGE)/2
 /*Data types Defines ---------------------------------------------------------*/
 typedef enum PCTR_Return_Type
 {
@@ -41,6 +55,7 @@ typedef enum PCTR_EV_State
 void PCTR_init(void);
 void PCTR_SET_EV_STATE ( PCTR_EV_State state);
 void PCTR_PWM_Handller(void);
+void PCTR_Update_EV_State(uint16_t* ADC_Data);
 
 
 PCTR_Return_Type PCTR_Set_Duty_Cycle (uint8_t value);
--- Core/Src/Pilot_Cntr.c
+++ Core/Src/Pilot_Cntr.c
@@ -11,7 +11,7 @@ static uint8_t PCTR_Duty_Cycle;
 static uint8_t PCTR_PWM_Counter;
 static PCTR_Init_Status is_PCTR_initialized = PCTR_NOT_INITIALIZED;
 PCTR_EV_State EV_State;
-
+uint16_t voltage;
 void PCTR_init(void)
 {
 	HAL_GPIO_WritePin(PWM_PIN_BANK, PWM_PIN, GPIO_PIN_SET);
@@ -20,11 +20,46 @@ void PCTR_init(void)
 	PCTR_Duty_Cycle = 100;
 
 	is_PCTR_initialized = PCTR_INITIALIZED;
+
+}
+
+void PCTR_Update_EV_State(uint16_t* ADC_Data)
+{
+
+	voltage = (ADC_Data[0] * VCC_IN) / ADC_MAX_VALUE;
+
+	if	(voltage >= MIN_NOT_CONNECTED_VOLTAGE )
+	{
+		EV_State = EV_NOT_CONNECTED;
+		PCTR_Set_Duty_Cycle(90); //for debugging
+	}
+	else if(voltage >= MIN_CONNECTED_VOLTAGE )
+	{
+		EV_State = EV_CONNECTED;
+		PCTR_Set_Duty_Cycle(70); //for debugging
+	}
+	else if(voltage >= MIN_CHARGING_VOLTAGE )
+	{
+		EV_State = EV_CHARGING;
+		PCTR_Set_Duty_Cycle(50); //for debugging
+	}
+	else if(voltage >= MIN_VENT_REQ_VOLTAGE )
+	{
+		EV_State = EV_CHARGING_VENT_REQ;
+		PCTR_Set_Duty_Cycle(30); //for debugging
+	}
+	else
+	{
+		EV_State = EV_ERROR;
+		PCTR_Set_Duty_Cycle(10); //for debugging
+	}
+
 }
 
 void PCTR_SET_EV_STATE ( PCTR_EV_State state)
 {
 	EV_State = state;
+
 }
 
 PCTR_Return_Type PCTR_Set_Duty_Cycle (uint8_t value)
@@ -46,6 +81,7 @@ PCTR_Return_Type PCTR_Set_Duty_Cycle (uint8_t value)
 
 void PCTR_PWM_Handller(void)
 {
+
 	if ( (is_PCTR_initialized == PCTR_INITIALIZED) && ( (EV_State == EV_CONNECTED) || (EV_State == EV_CHARGING) ||(EV_State == EV_CHARGING_VENT_REQ)  ))
 	{
 		if (PCTR_PWM_Counter <= 100)
